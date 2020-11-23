c105471cdaae6ee0223ff796191541eef1ee6c71
emCog <robin.leonard@gmail.com>
2020-11-23 11:05:17 +0000

    Completed first task
    Added behaviour to class

--- classes_intro/app.py
+++ classes_intro/app.py
@@ -12,3 +12,14 @@ from modules.bank_account import *
 # create instance of bank account class
 bank_account = BankAccount('John', 500, 'Business')
 print(bank_account.holder_name)
+
+
+bank_account.holder_name = 'Ada'
+print(bank_account.holder_name)
+
+
+bank_account.pay_in(50)
+print(bank_account.balance)
+
+bank_account.pay_monthly_fee()
+print(bank_account.balance)
--- classes_intro/modules/bank_account.py
+++ classes_intro/modules/bank_account.py
@@ -1,3 +1,7 @@
+#example function tied to example dictionary
+# def get_account_name(account):
+    # return account['holder_name']
+
 class BankAccount:
     #self refers to the instance which is being created
     # instance variables – follow self – have scope as long as the class instance is live
@@ -7,6 +11,11 @@ class BankAccount:
         #if python runs then it works, in this case ignore syntax highlighting
         self.type = type
 
-#example function tied to example dictionary
-# def get_account_name(account):
-    # return account['holder_name']
+    # example behaviour in class
+    def pay_in(self, amount):
+        self.balance += amount
+
+    # [Task:] Make a pay_monthly_fee method, which reduces the value of the account by 50. Try calling it in `app.py`
+
+    def pay_monthly_fee(self):
+        self.balance -= 50
