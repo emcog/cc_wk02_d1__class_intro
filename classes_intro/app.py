16a375dae62b568cd3ded5d8019c03ee5ff71b12
emCog <robin.leonard@gmail.com>
2020-11-23 10:05:56 +0000

    adding methods to class

--- classes_intro/app.py
+++ classes_intro/app.py
@@ -1,9 +1,13 @@
 from modules.bank_account import *
 
-account = {
-    'holder_name': 'John',
-    'balance': 500,
-    'type': 'business'
-}
+# example data structure
+# account = {
+#     'holder_name': 'John',
+#     'balance': 500,
+#     'type': 'business'
+# }
 
-print(get_account_name(account))
+# print(get_account_name(account))
+
+# create instance of bank account class
+bank_account = BankAccount('John', 500, 'Business')
--- classes_intro/modules/bank_account.py
+++ classes_intro/modules/bank_account.py
@@ -1,2 +1,7 @@
+class BankAccount:
+    def __init__(self, input_holder_name, input_balance, input_type):
+        pass
+
+
 def get_account_name(account):
     return account['holder_name']
